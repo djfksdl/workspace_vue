f254f88b2ccba850af9e51d3c213b4b772e49494
djfksdl <djfksdl@naver.com>
2024-03-20 16:06:09 +0900

    뷰

---
+++ vue-exam/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <!--여기에 있는 메뉴는 안쓰기때문에 지움 + style도 지움-->
+  <router-view/>
+</template>
+
+<style>
+
+</style>
---
+++ vue-exam/src/views/Ex01View.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>vue공부</h1>
+    <h2>{{ subTitle }}</h2>
+    <p>{{ content }}</p>
+</template>
+<script>
+    export default{
+        name: "Ex01View",
+        components:{},
+        data(){
+            return {//객체형테이다.
+                subTitle:"바인딩된 소제목", //필드(키):값
+                content:"오늘은 수요일~"
+            };
+        },
+        methods: {}
+    };
+</script>
+<style></style>
