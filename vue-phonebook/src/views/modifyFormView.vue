b9443fca5401bf9d78a8d116226ab8905ab035b7
djfksdl <djfksdl@naver.com>
2024-03-25 10:47:07 +0900

    vue-phonebook

---
+++ vue-phonebook/src/views/modifyFormView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+ </template>
+ ​
+ ​
+ <script>
+ export default {
+    name: "파일",
+    components: {},
+    data() {
+        return {};
+    },
+    methods: {},
+    created(){}
+ };
+ </script>
+ ​
+ ​
+ <style></style>
