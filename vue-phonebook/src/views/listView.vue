3a8eeef731f0de1df756695434e1752949bdbcfa
djfksdl <djfksdl@naver.com>
2024-03-25 20:37:35 +0900

    phonebook-list

--- vue-phonebook/src/views/listView.vue
+++ vue-phonebook/src/views/listView.vue
@@ -6,6 +6,8 @@
 
         <p>등록된 전화번호 리스트입니다.</p>
 
+        <div >
+            <button type="button" v-on:click="getList">리스트 가져오기</button>
             <table border="1">
                 <colgroup>
                     <col style="width: 100px;">
@@ -35,21 +37,41 @@
                 </tbody>	
             </table>
             <br>
-        
+        </div>
+
         <a href="">등록폼 이동</a>
     </div>
 </template>
 <script>
+import axios from'axios';
 export default {
-   name: "listView",
-   components: {},
-   data() {
-       return {};
-   },
-   methods: {
+    name: "listView",
+    components: {},
+    data() {
+        return {};
+    },
+    methods: {
+        getList(){
+            //console.log("클릭성공");
+            axios({
+                method: 'get', // put, post, delete 
+                url: 'http://localhost:9000/api/phonebooks',
+                headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
+                //params: guestbookVo, //get방식 파라미터로 값이 전달
+                //data: guestbookVo, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+                responseType: 'json' //수신타입
+            }).then(response => {
+                console.log(response); //수신데이타
+                
+            }).catch(error => {
+                console.log(error);
+            });
 
-   },
-   created(){}
+
+
+        }
+    },
+    created(){}
 };
 </script>
 <style></style>
