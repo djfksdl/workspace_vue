d22e09bb2b2e41800336487c4fe840d8663b47be
djfksdl <djfksdl@naver.com>
2024-04-03 18:10:23 +0900

    .

--- vue-exam/src/views/Ex09View.vue
+++ vue-exam/src/views/Ex09View.vue
@@ -5,6 +5,10 @@
     <p v-if="age<20">당신은 미성년자입니다.</p>
     <p v-else-if="age>=20 && age<65">당신은 성인입니다.</p>
     <p v-else>당신은 노인입니다.</p>
+    <div>
+        <input type="date" v-model="selectedDate" >
+        <p>선택한 날짜: {{ selectedDate }}</p>
+    </div>
 
 
 </template>
@@ -16,10 +20,12 @@ export default{
         return{     
             title:"Vue공부",
             subTitle:"if문",   
-            age: 75
+            age: 75,
+            selectedDate: '', // 선택한 날짜를 저장할 데이터
         }
     },
     methods:{
+        
     },
 }
 </script>
